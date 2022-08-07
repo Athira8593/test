2e3fea4f290265bcaebbf2ce0643d9593f0e5de0
Athira8593 <athiraramesh8593@gmail.com>
2022-08-07 18:02:08 +0530

    8

--- main.py
+++ main.py
@@ -2,13 +2,10 @@
 def print_hi(name):
     print(f'Hi, {name}')
     print('kjkjhkhkjh')
+    print("jjjjjhghgjjjj")
     print("jjjjjjjj")
-    print("jjjjjjjj")
-    print("jjjjjjjj")
-    print('kjkjhkhkjh')
-    print('kjkjhkhkjh')
 
-    print('kjkjhkhkjh')
+    print('kjiuukjhkhkjh')
 if __name__ == '__main__':
     print_hi('PyCharm')
 
